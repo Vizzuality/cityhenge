0741522db89e5457fe28312135423d3c0676a18d
Javier Arce <javierarce@gmail.com>
2013-07-11 15:42:09 +0200

    Adds slider

--- js/app_map.js
+++ js/app_map.js
@@ -1,3 +1,18 @@
-cartodb.createVis('mapContainer', 'http://viz2.cartodb.com/api/v1/viz/new_york_osm_line_clean/viz.json')
-    .done(function(vis, layers) {
-    });
+$(function() {
+
+var months = [];
+
+$( "#slider .handle" ).draggable({
+  containment: "parent",
+  axis: "x",
+  drag: function(e) {
+    console.log($(e.target).position().left);
+    var w = $(e.target).position().left
+    $(".highlight").width(w + 20);
+  },
+});
+
+var vis = cartodb.createVis('mapContainer', 'http://viz2.cartodb.com/api/v1/viz/new_york_osm_line_clean/viz.json');
+vis.done(function(vis, layers) { });
+
+});
