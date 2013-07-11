094f1fdb75611b1206d20a2f732e4d0da1a12dbf
Javier Arce <javierarce@gmail.com>
2013-07-11 15:51:05 +0200

    fixes border

--- js/app_map.js
+++ js/app_map.js
@@ -2,14 +2,15 @@ $(function() {
 
 var months = [];
 
+function onDrag(e) {
+  var w = $(e.target).position().left
+  $(".highlight").width(w + 20);
+}
+
 $( "#slider .handle" ).draggable({
   containment: "parent",
   axis: "x",
-  drag: function(e) {
-    console.log($(e.target).position().left);
-    var w = $(e.target).position().left
-    $(".highlight").width(w + 20);
-  },
+  drag: onDrag
 });
 
 var vis = cartodb.createVis('mapContainer', 'http://viz2.cartodb.com/api/v1/viz/new_york_osm_line_clean/viz.json');
