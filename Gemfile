60174418ab65779f4aae54f83147dc2cf5c2f162
Javier Arce <javierarce@gmail.com>
2013-07-12 13:03:32 +0200

    Animation

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source :rubygems
 
 gem 'rb-fsevent', '~> 0.9'
-gem 'compass', '>= 0.13.alpha.0'
+gem 'compass'
 gem 'breakpoint'
-gem 'animate'
--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
 # Require any additional compass plugins here.
 require 'breakpoint'
-require 'animate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
